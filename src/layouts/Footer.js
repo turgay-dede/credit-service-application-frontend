b8109717e43442a88c9c81b3d0db4ba41f1836de
turgay-dede <turgay.dede28@hotmail.com>
2022-02-26 16:59:29 +0300

    first commit

---
+++ src/layouts/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+        <footer>
+    <div className="container">
+      <div clclassNameass="col-lg-12">
+        <div className="copyright">
+          <p>
+            Copyright © 2022. All Rights Reserved.
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  
+    </div>
+  )
+}
---
+++ src/layouts/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a className="navbar-brand" href="#">INNOVA BANK</a>
+    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Anasayfa <span class="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Kullanıcılar</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Hesaplar</a>
+      </li>
+    </ul> 
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Kredi Başvurusu Yap</button>   
+  </div>
+</nav>
+    </div>
+  )
+}
